cfd049adb9e9d8da8e76adbf4e9e4bb8f02e08c5
PatrickTDewey <ptddev@protonmail.com>
2021-10-20 09:32:16 -0700

    initial commit

---
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -0,0 +1,127 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.myapplication.databinding.FragmentFirstBinding;
+
+import java.util.Objects;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+    private TextView showCountTextView;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+
+
+        return binding.getRoot();
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void countMe(View view) {
+        // Get val of text view
+        String countString = showCountTextView.getText().toString();
+        // Convert to number and increment
+        Integer count = Integer.parseInt(countString);
+        count++;
+        // Display the new value in the text view
+        showCountTextView.setText(Integer.toString(count));
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        showCountTextView = binding.textviewFirst;
+        SQLiteDatabase my_db = requireActivity().openOrCreateDatabase("banana.db", 0, null);
+        Cursor query = my_db.rawQuery("SELECT * FROM user;", null);
+        if (query.moveToFirst()) {
+            showCountTextView.setText(query.getString(0));
+            System.out.println(showCountTextView.getText().toString());
+        }
+        query.close();
+        my_db.close();
+        binding.randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int currentCount = Integer.parseInt(showCountTextView.getText().toString());
+
+                FirstFragmentDirections.ActionFirstFragmentToSecondFragment action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount);
+
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(action);
+            }
+        });
+        binding.toastButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast myToast = Toast.makeText(getActivity(), "Hello toast!", Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+        binding.countButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                countMe(view);
+            }
+
+        });
+
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+//        outState.putString("currentCount", showCountTextView.getText().toString());
+        super.onSaveInstanceState(outState);
+
+    }
+
+
+    @Override
+    public void onViewStateRestored( Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+//        if (savedInstanceState != null){
+//            String savedString = savedInstanceState.getString("currentCount");
+//            showCountTextView.setText(savedString);
+//        }
+    }
+
+    @Override
+    public void onPause() {
+        SQLiteDatabase my_db = requireActivity().openOrCreateDatabase("banana.db", 0, null);
+        String sql = "UPDATE user SET count = "+showCountTextView.getText().toString()+";" ;
+        my_db.execSQL(sql);
+        my_db.close();
+        super.onPause();
+
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.myapplication;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.PersistableBundle;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SQLiteDatabase sqLiteDatabase = getBaseContext().openOrCreateDatabase("banana.db", MODE_PRIVATE, null);
+
+        String sql = "CREATE TABLE IF NOT EXISTS user(count TEXT);";
+        sqLiteDatabase.execSQL(sql);
+        sqLiteDatabase.close();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+    }
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
